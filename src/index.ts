d93158316cf427836b4f3f8d5b473c2df43c8cbe
Marcelo Travasso <marceloctj@gmail.com>
2020-12-02 12:30:32 -0300

    chore: Muda index.ts para atender a lib

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
 import WhereBuilder from './WhereBuilder';
 
-const filtros = [
-  {campo: 'autor', valores: ['1', '2', '3']},
-  {campo: 'tipoPrato', valores: ['Entrada', 'Prato Principal']},
-]
-
-const where = WhereBuilder.create();
-
-filtros.forEach(({ campo, valores }) => {
-  where.and(campo, 'in', valores);
-});
-
-console.log(where.handle());
+export { WhereBuilder };
